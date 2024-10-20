32460b5760e05c821b812e0c7a646b814627a909
Oscar Fooley <oscarfoley05@gmail.com>
2024-10-20 09:01:47 +0200

    added three different colour modes

--- script.js
+++ script.js
@@ -249,3 +249,46 @@ function formatSeconds(seconds) {
     return `${hours} timmar, ${minutes} minuter, och ${seconds} sekunder`;
 }
 
+
+
+
+
+const lightModeRadio = document.getElementById('value-1');
+const darkModeRadio = document.getElementById('value-2');
+const hackerModeRadio = document.getElementById('value-3');
+
+//Function to apply the appropriate mode class
+function applyMode(mode) {
+  document.body.classList.remove('light-mode', 'dark-mode', 'hacker-mode');
+
+  if (mode === 'light') {
+    document.body.classList.add('light-mode');
+  } else if (mode === 'dark') {
+    document.body.classList.add('dark-mode');
+  } else if (mode === 'hacker') {
+    document.body.classList.add('hacker-mode');
+  }
+}
+
+lightModeRadio.addEventListener('change', () => {
+  if (lightModeRadio.checked) {
+    applyMode('light');
+  }
+});
+
+darkModeRadio.addEventListener('change', () => {
+  if (darkModeRadio.checked) {
+    applyMode('dark');
+  }
+});
+
+hackerModeRadio.addEventListener('change', () => {
+  if (hackerModeRadio.checked) {
+    applyMode('hacker');
+  }
+});
+
+if (darkModeRadio.checked) {
+  applyMode('dark');
+}
+
